32b3d19577caba7eecd0a956dfdc37fbabe8898d
icealys6 <icealys6@gmail.com>
2018-11-05 00:49:32 -0600

    MckRealty

---
+++ MckRealty/src/app/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+    /*const imgArray = new Array();
+
+    imgArray[0] = new Image();
+    imgArray[0].src = '../../assets/img/About/WelcomeAboardMRIroypic.jpg';
+    imgArray[1] = new Image();
+    imgArray[1].src = '../../assets/img/About/Chandrell2-1024x768.jpg';
+    imgArray[2] = new Image();
+    imgArray[2].src = '../../assets/img/About/Chandrell2-1024x768.jpg';
+    imgArray[3] = new Image();
+    imgArray[3].src = '../../assets/img/About/Chandrell2-1024x768.jpg';
+    */
+  }
+}
